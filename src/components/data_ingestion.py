15a3ebb2123498943f34f0fff64004ac861e7217
PiyushGopal <piyushgopal172@gmail.com>
2024-03-12 20:43:10 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,4 @@
-##this has all the code regarding reading the file
+##this file is used to split the data in order to train the model
 ## to run this file use this command:  python -m src.components.data_ingestion
 import os
 import sys
@@ -9,6 +9,9 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 
 @dataclass   ##Decorators can be used to add functionality to existing functions without modifying their source code.
@@ -50,4 +53,8 @@ class DataIngestion:
     
 if __name__=="__main__":
      obj=DataIngestion()
-     obj.initiate_data_ingestion()
+     train_data,test_data = obj.initiate_data_ingestion()
+     
+     data_transformation=DataTransformation()
+     data_transformation.initiate_data_transformation(train_data,test_data)
+     
